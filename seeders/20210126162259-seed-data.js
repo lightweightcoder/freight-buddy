cfc562d3da8812435457fea0a3a7fbf4ff797e4d
aljt <alvinljt@gmail.com>
2021-01-27 00:29:57 +0800

    added country-list package and empty seed file

---
+++ seeders/20210126162259-seed-data.js
@@ -0,0 +1,24 @@
+const { getNames } = require('country-list');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
