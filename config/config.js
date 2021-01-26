4203904ae19130bb55ca6eca24eb52eff1ce7047
aljt <alvinljt@gmail.com>
2021-01-26 16:51:54 +0800

    added username and development database name

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: '<YOUR_USER_NAME>',
+    username: 'aljt',
     password: null,
-    database: '<YOUR_APP_NAME>_development',
+    database: 'freight_buddy_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
