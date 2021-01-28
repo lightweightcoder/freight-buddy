4c9e75da24cd002aecf985595777557a0872cadd
aljt <alvinljt@gmail.com>
2021-01-28 21:10:36 +0800

    added navbar when there is no logged in user

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function TopNavbar({ user }) {
   const loggedInNavbar = (
@@ -28,9 +27,30 @@ export default function TopNavbar({ user }) {
     </Navbar>
   );
 
+  const loggedOutNavbar = (
+    <Navbar collapseOnSelect expand="md" bg="light" variant="light" className="top-navbar">
+      <Navbar.Brand href="#home">
+        <img
+          src="/package.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="Freight Buddy"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav id="items">
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="/register">Register</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+
   return (
     <div>
-      {loggedInNavbar}
+      {user ? loggedInNavbar : loggedOutNavbar}
     </div>
   );
 }
