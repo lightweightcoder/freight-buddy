6dc2cc7b4d027c6e9d804239de7dd14a9fefa9a9
aljt <alvinljt@gmail.com>
2021-01-29 10:05:57 +0800

    removed comments

--- src/components/RequestCard.jsx
+++ src/components/RequestCard.jsx
@@ -5,30 +5,6 @@ export default function RequestCard({
   photo, productName, price, country,
 }) {
   return (
-  // {/* <Carousel
-  //   swipeable={false}
-  //   draggable={false}
-  //   showDots
-  //   responsive={responsive}
-  //   ssr // means to render carousel on server-side.
-  //   infinite
-  //   autoPlay={this.props.deviceType !== 'mobile'}
-  //   autoPlaySpeed={1000}
-  //   keyBoardControl
-  //   customTransition="all .5"
-  //   transitionDuration={500}
-  //   containerClass="carousel-container"
-  //   removeArrowOnDeviceType={['tablet', 'mobile']}
-  //   deviceType={this.props.deviceType}
-  //   dotListClass="custom-dot-list-style"
-  //   itemClass="carousel-item-padding-40-px"
-  // >
-  //   <div>Item 1</div>
-  //   <div>Item 2</div>
-  //   <div>Item 3</div>
-  //   <div>Item 4</div>
-  // </Carousel>
-  // ; */}
     <div className="col-4 col-md-3">
       <a href="#linktorequest" className="request-card-link">
         <Card className="request-card">
