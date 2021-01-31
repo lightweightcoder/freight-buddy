696d79f3c9276497c14797a15e2b5958674062b3
aljt <alvinljt@gmail.com>
2021-01-31 20:50:08 +0800

    refactored code to query for a request detail right after a request is clicked on the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import React, { useState } from 'react';
+import axios from 'axios';
 
 // import components
 import TopNavbar from './components/TopNavbar.jsx';
@@ -15,22 +16,30 @@ const pages = {
 export default function App() {
   const [page, setPage] = useState(pages.HOME);
   const [user, setUser] = useState(null);
-  const [selectedRequestId, setSelectedRequestId] = useState(null);
+  const [selectedRequest, setSelectedRequest] = useState(null);
 
-  // set a selected request id, then set the state of the page to
+  // get the details of the selected request, then set the state of the page to
   // to display the request details
   // this occurs when a potential helper clicks on a request at the homepage
   const selectAndViewARequestPageHelper = (id) => {
-    setSelectedRequestId(id);
+    // get the request's details from the DB and set it in selectedRequest variable
+    axios.get(`/requests/${id}`)
+      .then((result) => {
+        console.log('request/:id result is', result);
 
+        // set the request's details
+        setSelectedRequest(result.data.request);
+      });
+
+    // set the page to view the selected request's details
     setPage(pages.SHOW_REQUEST_HELPER_VIEW);
   };
 
   return (
     <div>
       <TopNavbar user={user} setPage={setPage} />
-      {(page === pages.HOME) ? <HomePage setUser={setUser} setSelectedRequestId={setSelectedRequestId} selectAndViewARequestPageHelper={selectAndViewARequestPageHelper} /> : ''}
-      {(page === pages.SHOW_REQUEST_HELPER_VIEW ? <RequestHelperView selectedRequestId={selectedRequestId} /> : '')}
+      {(page === pages.HOME) ? <HomePage setUser={setUser} selectAndViewARequestPageHelper={selectAndViewARequestPageHelper} /> : ''}
+      {(page === pages.SHOW_REQUEST_HELPER_VIEW ? <RequestHelperView selectedRequest={selectedRequest} /> : '')}
     </div>
   );
 }
--- src/components/RequestHelperView.jsx
+++ src/components/RequestHelperView.jsx
@@ -1,67 +1,50 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
 
-export default function RequestHelperView({ selectedRequestId }) {
-  const [requestDetails, setRequestDetails] = useState(null);
+export default function RequestHelperView({ selectedRequest }) {
   // variable to store JSX of the carousel items
-  const [carouselItemsList, setCarouselItemsList] = useState(null);
+  let carouselItems = '';
+  const [buttonText, setButtonText] = useState(null);
 
-  // when request details (in helper view) page renders for the 1st time
-  // get the request's details from the DB and set it in state variable
-  useEffect(() => {
-    axios.get(`/requests/${selectedRequestId}`)
-      .then((result) => {
-        console.log('request/:id result is', result);
+  if (selectedRequest === null) {
+    return <div />;
+  }
 
-        // set the request's details
-        setRequestDetails(result.data.request);
-
-        console.log('length is', result.data.request.productPhotos.length);
-        // create the photos of the request's product
-        if (result.data.request.productPhotos.length > 0) {
-          const carouselItems = result.data.request.productPhotos.map((photo, index) => (
-            <Carousel.Item key={photo.filename}>
-              <img
-                className="d-block w-100"
-                src={photo.filename}
-                alt={`${index + 1} slide`}
-              />
-            </Carousel.Item>
-          ));
-
-          setCarouselItemsList(carouselItems);
-        } else {
-          // if there is no photo uploaded by requester, give it a default image
-          const carouselItems = (
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg"
-                alt="First slide"
-              />
-            </Carousel.Item>
-          );
-
-          setCarouselItemsList(carouselItems);
-        }
-      });
-  }, []);
+  // create the photos of the request's product
+  if (selectedRequest.productPhotos.length > 0) {
+    carouselItems = selectedRequest.productPhotos.map((photo, index) => (
+      <Carousel.Item key={photo.filename}>
+        <img
+          className="d-block w-100"
+          src={photo.filename}
+          alt={`${index + 1} slide`}
+        />
+      </Carousel.Item>
+    ));
+  } else {
+    // if there is no photo uploaded by requester, give it a default image
+    carouselItems = (
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg"
+          alt="First slide"
+        />
+      </Carousel.Item>
+    );
+  }
 
   const nextIcon = <span aria-hidden="true" className="fas fa-angle-right fa-2x" />;
   const prevIcon = <span aria-hidden="true" className="fas fa-angle-left fa-2x" />;
-
-  if (requestDetails === null) {
-    return <div />;
-  }
 
   return (
     <div className="container">
       <div className="row">
         { /* product name and request id */ }
         <div className="col-12 product-name">
-          <h4>{requestDetails.productName}</h4>
-          <p><small>{`Request id: ${requestDetails.id}`}</small></p>
+          <h4>{selectedRequest.productName}</h4>
+          <p><small>{`Request id: ${selectedRequest.id}`}</small></p>
         </div>
       </div>
       <div className="row">
@@ -70,7 +53,7 @@ export default function RequestHelperView({ selectedRequestId }) {
           <div className="row d-flex justify-content-center align-self-center">
             <div className="col-6 col-sm-12">
               <Carousel prevLabel="" nextLabel="" nextIcon={nextIcon} prevIcon={prevIcon}>
-                { carouselItemsList }
+                { carouselItems }
               </Carousel>
             </div>
           </div>
@@ -81,15 +64,15 @@ export default function RequestHelperView({ selectedRequestId }) {
           <div className="row">
             <div className="col-12">
               <p className="offered-price-heading"><small>offered price:</small></p>
-              <h4>{`$${requestDetails.price}`}</h4>
-              <p>{`Requester: ${requestDetails.requester.name}`}</p>
-              <p>{`Country of Purchase: ${requestDetails.country.name}`}</p>
+              <h4>{`$${selectedRequest.price}`}</h4>
+              <p>{`Requester: ${selectedRequest.requester.name}`}</p>
+              <p>{`Country of Purchase: ${selectedRequest.country.name}`}</p>
               <p>
                 Reference Link:
                 {' '}
                 <br />
                 <small>
-                  <a href={requestDetails.referenceLink}>{requestDetails.referenceLink}</a>
+                  <a href={selectedRequest.referenceLink}>{selectedRequest.referenceLink}</a>
                 </small>
               </p>
             </div>
@@ -99,14 +82,14 @@ export default function RequestHelperView({ selectedRequestId }) {
       <div className="row">
         <div className="col-12">
           { /* somemore request details */ }
-          <p>{`Description: ${requestDetails.description}`}</p>
-          <p>{`Shipping Address: ${requestDetails.shippingAddress}`}</p>
-          <p><b>{`Status: ${requestDetails.status}`}</b></p>
+          <p>{`Description: ${selectedRequest.description}`}</p>
+          <p>{`Shipping Address: ${selectedRequest.shippingAddress}`}</p>
+          <p><b>{`Status: ${selectedRequest.status}`}</b></p>
         </div>
       </div>
       <div className="row">
         <div className="col-12">
-          <button type="button" className="btn btn-primary">hi</button>
+          <button type="button" className="btn btn-primary">confirm</button>
         </div>
       </div>
     </div>
